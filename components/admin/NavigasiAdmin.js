99811ea5429f394bd8cea7b4e5aea69b53291d24
Haris Sucipto <harisssucipto@gmail.com>
2019-03-07 11:25:44 +0700

    tambah navigasi admin

--- components/admin/NavigasiAdmin.js
+++ components/admin/NavigasiAdmin.js
@@ -6,21 +6,89 @@ const { SubMenu } = Menu;
 
 const NavigasiAdmin = () => (
   <>
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={['sub1-1']}>
       <SubMenu
         key="sub1"
         title={
           <span>
             <Icon type="user" />
-            Akun
+            Profil
           </span>
         }
       >
-        <Menu.Item key="1" onClick={() => Router.push('/admin/profil')}>
-          Info Akun
+        <Menu.Item key="sub1-1" onClick={() => Router.push('/admin/profil')}>
+          Info Profil
         </Menu.Item>
-        <Menu.Item key="2" onClick={() => Router.push('/admin/setting')}>
-          Pengaturan Akun
+        <Menu.Item key="sub-1-2" onClick={() => Router.push('/admin/setting')}>
+          Setting Profil
+        </Menu.Item>
+      </SubMenu>
+
+      <SubMenu
+        key="sub2"
+        title={
+          <span>
+            <Icon type="user" />
+            Dosen
+          </span>
+        }
+      >
+        <Menu.Item key="sub2-1" onClick={() => Router.push('/admin/dosen')}>
+          Kelola Dosen
+        </Menu.Item>
+        <Menu.Item key="sub2-2" onClick={() => Router.push('/admin/dosen/tambah')}>
+          Tambah Dosen
+        </Menu.Item>
+      </SubMenu>
+
+      <SubMenu
+        key="sub3"
+        title={
+          <span>
+            <Icon type="user" />
+            Mahasiswa
+          </span>
+        }
+      >
+        <Menu.Item key="sub3-1" onClick={() => Router.push('/admin/mahasiswa/')}>
+          Kelola Mahasiswa
+        </Menu.Item>
+        <Menu.Item key="sub3-2" onClick={() => Router.push('/admin/mahasiswa/tambah')}>
+          Tambah Mahasiswa
+        </Menu.Item>
+      </SubMenu>
+
+      <SubMenu
+        key="sub4"
+        title={
+          <span>
+            <Icon type="user" />
+            Mata Kuliah
+          </span>
+        }
+      >
+        <Menu.Item key="sub4-1" onClick={() => Router.push('/admin/mata-kuliah/')}>
+          Kelola Mata Kuliah
+        </Menu.Item>
+        <Menu.Item key="sub4-2" onClick={() => Router.push('/admin/mata-kulia/tambah')}>
+          Tambah Mata Kuliah
+        </Menu.Item>
+      </SubMenu>
+
+      <SubMenu
+        key="sub5"
+        title={
+          <span>
+            <Icon type="user" />
+            Kelas
+          </span>
+        }
+      >
+        <Menu.Item key="sub5-1" onClick={() => Router.push('/admin/kelas/')}>
+          Kelola Kelas
+        </Menu.Item>
+        <Menu.Item key="sub5-2" onClick={() => Router.push('/admin/kelas/tambah')}>
+          Tambah Kelas
         </Menu.Item>
       </SubMenu>
     </Menu>
