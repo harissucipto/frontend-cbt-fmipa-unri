5362b80b05e65f5ca4141bb559115ac0074dd863
Haris Sucipto <harisssucipto@gmail.com>
2019-05-06 02:12:56 +0700

    tambah server prod

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = 'http://localhost:4444';
+export const prodEndpoint = 'https://cbt-fmipa-ur-5671325706.herokuapp.com/cbt-fmipa-ur/prod';
 export const perPage = 4;
